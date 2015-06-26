1e9d9c7210ffc21a2d1adfad3b8600cbc5136515
Rodrigo Altamirano <rodrigo.altamirano@gmail.com>
2015-06-26 08:28:53 -0300

    First try at modeling a chain-of-responsibility of analyzers looking for tracklists on the description of the videos.

--- handlerMP3File/handler-file.js
+++ handlerMP3File/handler-file.js
@@ -1,33 +1,45 @@
-//Handler for work with mp3Files.
-var handlerFile = {
+var ChainOfResponsibility = require('chaining-tool');
 
-    extractTracklist: function(videoDescription) {
+exports.extractTracklistAnalyzers = new ChainOfResponsibility();
+
+// Add built-in tracklist analyzers
+exports.extractTracklistAnalyzers.add(function(context, next) {
+ 	var regex = /(.+)\s(\d{1,2}:\d{2})/gi;
+	var patternsFound = context.videoDescription.match(regex);
+	if (patternsFound && patternsFound.length > 0) {
+		for(var index=0; index<patternsFound.length; index++)
+			context.tracklist.push({
+				"title": patternsFound[index]
+			});
+
+		next(false); // Finish the analysis
+
+		next(false); // Finish the analysis
+	} else {
+		next(); // Try with next analyzer
+	}		
+});
+ 
+exports.extractTracklistAnalyzers.add(function(context, next) {
+    next(false); 
+});
+ 
+
+
+exports.extractTracklist = function(videoDescription) {
         var tracklist = [];
-        // TODO: extract tracklist from video's description.
-        // I'm thinking on some kind of 'chain-of-responsibility' for handlers that could 
-        // actually extract songs's title and start/stop markers. 
-        tracklist.push({
-            "title": "1st song",
-            "start": 0,
-            "end": 192
-        });
-        tracklist.push({
-            "title": "2nd song",
-            "start": 195,
-            "end": 402
-        });
-        tracklist.push({
-            "title": "3rd song",
-            "start": 404,
-            "end": 721
-        });
+	var context = { "videoDescription": videoDescription, "tracklist": tracklist };
+
+	exports.extractTracklistAnalyzers.start(context, function(context) {
+		return context.tracklist;
+	}, function(context) {
+		return context.tracklist;
+	});
 
         return tracklist;
-    },
-
-    splitMP3FileByTracklist: function(file, tracklist) {
-        console.log('TODO: splitMP3FileByTracklist: \nFile: \n' + file + "\n\nTracklist: \n" + JSON.stringify(tracklist));
-    }
 };
 
-module.exports = handlerFile;
+exports.splitMP3FileByTracklist = function(file, tracklist) {
+        console.log('TODO: splitMP3FileByTracklist: \nFile: \n' + file + "\n\nTracklist: \n" + JSON.stringify(tracklist));
+};
+
