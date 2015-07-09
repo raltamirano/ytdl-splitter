14c3614bf14296462bed2972e588416864a89003
Rodrigo Altamirano <rodrigo.altamirano@gmail.com>
2015-07-08 22:48:08 -0300

    Total refactor in order to use 'ytdl-splitter-core' module.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # ytdl-splitter
-Utility to download youtube videos (using youtube-dl) as separate audio files,
-using the tracklist provided on the description of the video or CUE files.
+Standalone utility to download youtube videos (using youtube-dl) as separate
+audio files, using the tracklist provided on the description of the video or
+CUE files.
 
 ## Usage
 
@@ -12,10 +13,14 @@ Extract audio files using a CUE file as the tracklist:
 
     nodejs index.js --cue-file tracklist.cue  <video_url>
 
-You can also set values for some metadata fields (overwritting values read from any tracklist source, if any):
+You can also set values for some metadata fields (overwritting values read from
+any tracklist source, if any):
 
     nodejs index.js -t "The Album" -a "The Artist" -y "2015" <video_url>
 
 ## Notes
 
-As this package depends on 'youtube-dl' and 'avconv', check for their requirements also. Currently, they are: the 'youtube-dl' and 'avconv' executables in your PATH, ready to be executed. Python (2.6, 2.7, or 3.2+) maybe needed in order to properly run 'youtube-dl'.
+As this package depends on 'ytdl-splitter-core', which in turns depends on modules
+'youtube-dl' and 'avconv', check for their requirements also. Currently, they are:
+the 'youtube-dl' and 'avconv' executables in your PATH, ready to be executed.
+Python (2.6, 2.7, or 3.2+) maybe needed in order to properly run 'youtube-dl'.
--- index.js
+++ index.js
@@ -1,72 +1,50 @@
-var fs = require('fs');
-var os = require('os');
-var path = require('path');
-var youtubedl = require('youtube-dl');
-var core = require('./handlerMP3File/handler-file.js');
 var program = require('commander');
+var Splitter = require('ytdl-splitter-core');
 
 program
     .usage('[options] <video_url>')
     .option('-c, --cue-file [file]', 'Use the specified .CUE file as the tracklist')
     .option('-t, --album-name [album name]', 'Album name')
-    .option('-a, --album-artist [album artist]', 'Album artist(s)')
+    .option('-a, --artist-name [artist name]', 'Album artist(s)')
     .option('-y, --album-year [album year]', 'Album year')
     .parse(process.argv);
 
 if (program.args.length != 1)
 	throw 'You must specify the URL of the video to download/split only!';
 
-if (program.cueFile)
-    core.addCUETracklistExtractor(program.cueFile);
-
 var url = program.args[0];
-console.log('Video URL: ' + url);
+var options = {};
 
-var options = [];
-youtubedl.getInfo(url, options, function(err, info) {
-	if (err) throw err;
-
-	// Let's somehow extract a tracklist from the video information we got,
-	// then extract each song as a separate audio file.
-    var videoContext = {
-        "description": info.description,
-        "duration": info.duration,
-        "durationInSecs": core.momentToSeconds(info.duration)
-    };
-
-    if (program.cueFile)
-        videoContext.cueFile = program.cueFile;
-    if (program.albumName)
-        videoContext.albumName = program.albumName;
-    if (program.artistName)
-        videoContext.artistName = program.artistName;
-    if (program.albumYear)
-        videoContext.albumYear = program.albumYear;
-
-	core.extractTracklist(videoContext, function(tracklist) {
-		if (!tracklist || !tracklist.tracks || tracklist.tracks.length <= 0)
-				throw 'No tracklist could be inferred/read!';
-
-        if (videoContext.albumName != undefined)
-            tracklist.albumName = videoContext.albumName;
-        if (videoContext.artistName != undefined)
-            tracklist.artistName = videoContext.artistName;
-        if (videoContext.albumYear != undefined)
-            tracklist.albumYear = videoContext.albumYear;
-
-		var video = new youtubedl(url);
-		var inputFileRoot = path.join(os.tmpdir(), info._filename);
-		fs.mkdirSync(inputFileRoot);
-		var inputFile = path.join(inputFileRoot, info._filename);
-
-		video.pipe(fs.createWriteStream(inputFile));
-
-		video.on('end', function() {
-			core.splitFileByTracklist(inputFile, tracklist, function(returnCode) {
-				console.log('Finished!');
-				process.exit(returnCode);
-			});
-		});
-	});
-
+var splitter = new Splitter();
+splitter.on('ready', function() {
+    console.log('Ready to download and split videos!');
 });
+
+splitter.on('start', function(info) {
+    console.log(info.url, ' is about to be downloaded then splitted!');
+});
+
+splitter.on('end', function(info) {
+    console.log(info.url, 'was downloaded then splitted to files at:', info.outputPath);
+});
+
+splitter.on('message', function(message) {
+    console.log(message);
+});
+
+splitter.on('error', function(error) {
+    console.log('While processing', error.url, 'we had the following problems:', JSON.stringify(error));
+});
+
+
+if (program.cueFile)
+    splitter.addCUETracklistExtractor(program.cueFile);
+
+if (program.albumName)
+    options.albumName = program.albumName;
+if (program.artistName)
+    options.artistName = program.artistName;
+if (program.albumYear)
+    options.albumYear = program.albumYear;
+
+splitter.split(url, options);
